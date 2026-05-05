c6f24ed00bf721273b0ff796266a7a29a29dee3f
Yashaswini Chandrappa <97212256+yashaswini-chandrappa@users.noreply.github.com>
2026-05-05 23:05:09 +0530

    Delete Assignment_01 directory

--- Assignment_01/question_03.py
+++
@@ -1,9 +0,0 @@
-'''Q3. Ask the user to enter two integers and one float. Convert them all to floats
-and print their average.
-'''
-
-int_1 = int(input("Enter an integer: "))
-int_2 = int(input("Enter another integer: "))
-float_1 = float(input("Enter a float value: "))
-avg = (float(int_1)+float(int_2)+float_1)/3
-print(f"The average is : {avg}")
--- Assignment_01/question_05.py
+++
@@ -1,9 +0,0 @@
-'''Evaluate and print the result of the following expression:
-x = 10 + 3 * 2 ** 2
-Based on what you learnt in the lecture explain why the output is what it is'''
-
-x = 10 + 3 * 2 ** 2 
-#x = 10+3*4
-#x = 10+12
-#x = 22
-print(x)
