b7eb49d0c5a9c39c479fa6f1cc9439fd9d2d85d1
Yashaswini Chandrappa <97212256+yashaswini-chandrappa@users.noreply.github.com>
2026-05-05 23:05:31 +0530

    Delete Assignment_02 directory

--- Assignment_02/question_04.py
+++
@@ -1,8 +0,0 @@
-'''Q4. Write a function to count and return the number of digits in a number n,.'''
-
-n = int(input("Enter a digit: "))
-count = 0
-while n>0:
-    count +=1
-    n = n//10
-print("The number of digits in the number is: ",count)
--- Assignment_02/question_06.py
+++
@@ -1,6 +0,0 @@
-'''Q6. Write a program to print all numbers from 1 to 100 that are divisible by both 3 and 5.'''
-
-print("The Numbers divisible by both 3 and 5 in the range of 1 to 100 is :")
-for i in range (1,101):
-    if (i%3==0) and(i%5==0):
-        print(i,end =" ")
