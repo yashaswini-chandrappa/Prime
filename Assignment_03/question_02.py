0f57b9fa1acbb7ae7ac2ec7afb1fb124803a9024
Yashaswini Chandrappa <97212256+yashaswini-chandrappa@users.noreply.github.com>
2026-05-05 23:05:48 +0530

    Delete Assignment_03 directory

--- Assignment_03/question_02.py
+++
@@ -1,9 +0,0 @@
-'''Q2. Given a list of integers, compute the average of all numbers in the list.'''
-
-list1 = [1,23,34,465,123,121344]
-print(list1)
-print(len(list1))
-for i in list1:
-    sum =+ i
-    avg = sum/(len(list1))
-print("The Average of the list is :",avg)
